53079c96ecc76224ca14b8a20187968222d1ef75
shaurm8 <akskdhdsjaksv@gmail.com>
2026-09-21 16:34:58 +0700

    Update kartoshka.gd

--- kartoshka.gd
+++ kartoshka.gd
@@ -68,3 +68,4 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and body.has_method("take_damage"):
 		body.take_damage(damage)
 		print("4. Урон нанесен! Здоровье игрока: ", body.current_hp)
+#,liauuh
